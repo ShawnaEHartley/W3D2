1f24e4b208ea9594dfde582d67318d6e897e4e15
Zane <zeisen33@gmail.com>
2022-11-22 17:04:53 -0500

    render, populate working

--- MemoryGame/Board.rb
+++ MemoryGame/Board.rb
@@ -4,20 +4,24 @@ require_relative "Card.rb"
 
 class Board
 
+    attr_reader :board
+
     def initialize
         @board = Array.new(4) {Array.new(4)}
     end
 
     def [](pos)
-        @board[pos[0]][pos[1]]
+        row, col = pos
+        @board[row][col]
     end
 
     def []=(pos, card)
-        @board[pos[0]][pos[1]] = card
+        row, col = pos
+        @board[row][col] = card
     end
 
     def empty?(pos)
-        @board[pos[0]][pos[1]] == nil
+        self[pos] == nil
     end
 
     def any_empty?
@@ -26,22 +30,36 @@ class Board
         
     def populate
         face_values = [:L, :O, :P, :G, :H, :I, :W, :Y, :L, :O, :P, :G, :H, :I, :W, :Y]  
-        while any_empty?
-            face_values.each_with_index do |value, i|
-                new_card = Card.new(value)
-                row = rand(0...4)
-                col = rand(0...4)
-                pos = [row, col]
-                if empty?(pos)
-                    self.[]=(pos, new_card)
-                end
+        @board.each.with_index do |row, rowIdx|
+            row.each.with_index do |ele, colIdx|
+                pos = [rowIdx, colIdx]
+                idx = rand(0...face_values.length)
+                new_card = Card.new(face_values[idx])
+                self.[]=(pos, new_card)
+                face_values.delete_at(idx)
+            end    
+        end
+    end 
+    
+    def render
+        @board.each do |row|
+            row.each do |card|
+                print card.face_value
             end
+            print "\n"
         end
     end
-    
+
 end
+
 
 board = Board.new
 # p board.empty?([2,2])
+# p board.empty?([0,2])
+# board.[]=([0, 2], Card.new(:U))
 board.populate
-p board
+# p board.empty?([0, 2])
+# p board.board[0][2]
+board.render
+
+
